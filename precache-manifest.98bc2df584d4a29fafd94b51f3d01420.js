1a30d19e5ef73879fbf7cfc641976884007d5e75
Rahul Gaba <rahulgaba16@gmail.com>
2018-12-02 23:09:18 +0100

    Updates

---
+++ precache-manifest.98bc2df584d4a29fafd94b51f3d01420.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f274944183fd8df48a95",
+    "url": "/static/js/main.f2749441.chunk.js"
+  },
+  {
+    "revision": "9af00e7a9af2b26039d2",
+    "url": "/static/js/1.9af00e7a.chunk.js"
+  },
+  {
+    "revision": "f274944183fd8df48a95",
+    "url": "/static/css/main.237a02e9.chunk.css"
+  },
+  {
+    "revision": "455a64b1916ff7b943cd26de0ad692a2",
+    "url": "/index.html"
+  }
+];
